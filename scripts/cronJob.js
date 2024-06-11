f23c9d37782f8a5f3eb06a70572d730068704e35
FarahR01 <119661787+FarahR01@users.noreply.github.com>
2024-06-11 15:37:23 +0100

    Update cronJob.js

--- scripts/cronJob.js
+++ scripts/cronJob.js
@@ -1,7 +1,7 @@
 const cron = require('node-cron');
 const articleService = require('../services/articleService');
 
-cron.schedule('* * * * *', async () => {
+cron.schedule('0 * * * *', async () => {
   console.log('Vérification des nouveaux articles WordPress...');
   const latestArticles = await articleService.fetchLatestArticles();
   await articleService.saveNewArticles(latestArticles);
